3cf916335c95e1cc6c4bc62ac30d72d029c144e2
Jeonghyeon <gkswl412@naver.com>
2024-12-24 18:23:49 +0900

    feat: Add text field for entering tasks

--- lib/main.dart
+++ lib/main.dart
@@ -13,38 +13,91 @@ class MainApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
-          appBar: AppBar(
-            title: const Text("Custom Calendar"),
-          ),
-          body: DailyTaskFeedView(
-            selectedDate: DateTime.now(),
-            todoItems: [
-              TodoItem(
-                note: '본가에 폐가전 배출 요청 전화하기',
-                time: DateTime.now(),
-                date: '2024-12-20',
-                isDone: false,
+        appBar: AppBar(
+          title: const Text("Custom Calendar"),
+        ),
+        body: DailyTaskFeedView(
+          selectedDate: DateTime.now(),
+          todoItems: [
+            TodoItem(
+              note: '본가에 폐가전 배출 요청 전화하기',
+              time: DateTime.now(),
+              date: '2024-12-20',
+              isDone: false,
+            ),
+            TodoItem(
+              note: '영화 관람',
+              time: DateTime.now(),
+              date: '2024-12-20',
+              isDone: false,
+            ),
+            TodoItem(
+              note: '귀가후 카레에 밥 먹기',
+              time: DateTime.now(),
+              date: '2024-12-21',
+              isDone: false,
+            ),
+            TodoItem(
+              note: '개발 계획 세우기',
+              time: DateTime.now(),
+              date: '2024-12-23',
+              isDone: false,
+            ),
+          ],
+        ),
+        bottomSheet: Container(
+          color: Colors.white,
+          child: Row(
+            children: [
+              IconButton(
+                onPressed: () {
+                  debugPrint('open Calendar!');
+                },
+                icon: const Icon(
+                  Icons.calendar_month_rounded,
+                ),
               ),
-              TodoItem(
-                note: '영화 관람',
-                time: DateTime.now(),
-                date: '2024-12-20',
-                isDone: false,
+              Expanded(
+                child: Container(
+                  height: 40,
+                  margin: const EdgeInsets.symmetric(
+                    vertical: 7,
+                  ),
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(20),
+                    color: const Color(0xffededed),
+                  ),
+                  child: const TextField(
+                    cursorColor: Colors.blue,
+                    decoration: InputDecoration(
+                      hintText: '할 일 입력',
+                      hintStyle: TextStyle(
+                        color: Colors.grey,
+                        fontSize: 15,
+                      ),
+                      contentPadding: EdgeInsets.symmetric(
+                        vertical: 10,
+                        horizontal: 15,
+                      ),
+                      border: OutlineInputBorder(
+                        borderSide: BorderSide.none,
+                      ),
+                    ),
+                  ),
+                ),
               ),
-              TodoItem(
-                note: '귀가후 카레에 밥 먹기',
-                time: DateTime.now(),
-                date: '2024-12-21',
-                isDone: false,
-              ),
-              TodoItem(
-                note: '개발 계획 세우기',
-                time: DateTime.now(),
-                date: '2024-12-23',
-                isDone: false,
+              IconButton(
+                onPressed: () {
+                  debugPrint('Add!');
+                },
+                icon: const Icon(
+                  Icons.add,
+                ),
               ),
             ],
-          )),
+          ),
+        ),
+      ),
     );
   }
 }
